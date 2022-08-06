17fcfcb4270ee74258663a009a5aa37a65d19dd0
HareKrsna16 <97773473+HareKrsna16@users.noreply.github.com>
2022-08-07 03:57:08 +0530

    FLOW001

---
+++ FLOW001.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class FLOW001 {
+	public static void main (String args[]) {
+		int t;
+		Scanner sc = new Scanner(System.in);
+		t = sc.nextInt();
+		while(t-- != 0) {
+			System.out.println(sc.nextInt() + sc.nextInt());
+		}
+	}
+}
+
+
